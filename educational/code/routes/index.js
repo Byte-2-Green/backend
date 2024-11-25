fb8b3fc4849ec8de64ea474144ecc84973416103
Wesley <wesley06@zeelandnet.nl>
2024-11-25 13:45:13 +0100

    added back cors

--- educational/code/routes/index.js
+++ educational/code/routes/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import { responseFoodForThought, updateFoodForThought, responseByIdFoodForThought, responseByCategoryExample } from '../controllers/foodForThoughtController.js'; // Adjust the path to your file
 import { checkIfWork } from '../middleware/foodForThougthMiddelware.js';
-
+import cors from 'cors';
+const app = express();
 const router = express.Router();
+
+app.use(cors());
 
 // Route to get all food for thought
 router.get('/foodForThought', checkIfWork, responseFoodForThought);
