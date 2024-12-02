37b8c7cd2641590964343639534bdfa67cf31c99
popo0015 <popo0015@hz.nl>
2024-12-02 12:02:01 +0100

    Minor changes to the folder structure of the challenges microservice

--- challenges/code/routes/index.js
+++ challenges/code/routes/index.js
@@ -1,22 +1,9 @@
 import express from 'express';
-import { responseFoodForThought, updateFoodForThought, responseByIdFoodForThought, responseByCategoryExample } from '../controllers/foodForThoughtController.js'; // Adjust the path to your file
-import { checkIfWork } from '../middleware/foodForThougthMiddelware.js';
+import { test} from '../controllers/challengesController.js';
 import cors from 'cors';
 const app = express();
 const router = express.Router();
 
-router.get('/foodForThought', cors(), responseFoodForThought);
-
-// Route to get all food for thought
-router.get('/foodForThought', checkIfWork, responseFoodForThought);
-
-// Route to add a new food for thought
-router.post('/foodForThought', checkIfWork, updateFoodForThought);
-
-// Route to get a specific food for thought by ID
-router.get('/foodForThought/:id', checkIfWork, responseByIdFoodForThought);
-
-// Route to get all food for thought by category
-router.get('/foodForThought/category/:category', checkIfWork, responseByCategoryExample);
+router.get('/challenges', cors(), test);
 
 export default router;
