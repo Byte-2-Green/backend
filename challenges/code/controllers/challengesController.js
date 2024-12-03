438b5dc64ca1762c75b1f6c973312e22b59d6a3d
Wesley <wesley06@zeelandnet.nl>
2024-12-03 14:33:24 +0100

    added notification tabel

--- challenges/code/controllers/challengesController.js
+++ challenges/code/controllers/challengesController.js
@@ -11,3 +11,51 @@ export async function test(req, res) {
     res.status(200).json(results);
   });
 }
+
+// Get all notifications 
+export async function getNotifications(req, res) {
+  const query = 'SELECT * FROM Notifications';  // SQL query to fetch all records
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error fetching data:', err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.status(200).json(results);
+  });
+}
+
+// Get a notification by ID
+export async function getNotificationById(req, res) {
+  const query = 'SELECT * FROM Notifications WHERE id = ?';  // SQL query to fetch a record by
+  db.query(query, [req.params.id], (err, results) => {
+    if (err) {
+      console.error('Error fetching data:', err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.status(200).json(results);
+  });
+}
+
+// Create a new notification
+export async function createNotification(req, res) {
+  const query = 'INSERT INTO Notifications SET ?';  // SQL query to insert a new record
+  db.query(query, req.body, (err, results) => {
+    if (err) {
+      console.error('Error inserting data:', err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.status(201).json({ id: results.insertId });
+  });
+}
+
+// Get a notification by Challenge_ID
+export async function getNotificationByChallengeId(req, res) {
+  const query = 'SELECT * FROM Notifications WHERE Challenge_ID = ?';  // SQL query to fetch a record by
+  db.query(query, [req.params.id], (err, results) => {
+    if (err) {
+      console.error('Error fetching data:', err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.status(200).json(results);
+  });
+}
--- challenges/code/routes/index.js
+++ challenges/code/routes/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
-import { test} from '../controllers/challengesController.js';
+import { test, getNotificationByChallengeId, getNotificationById, getNotifications, createNotification} from '../controllers/challengesController.js';
 import cors from 'cors';
 const app = express();
 const router = express.Router();
 
 router.get('/challenges', cors(), test);
+
+router.get('/notifications', cors(), getNotifications);
+
+router.get('/notifications/:id', cors(), getNotificationById);
+
+router.get('/notifications/challenge/:id', cors(), getNotificationByChallengeId);
+
+router.post('/notifications', cors(), createNotification);
 
 export default router;
