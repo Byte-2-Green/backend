ee9a13f09470cbd70803012b0fa31f14820f2779
Alexandra <smir0002@hz.nl>
2024-12-03 13:43:01 +0100

    implemented new parameters in the table and fresh challenges in the seeder

--- challenges/mysql/initdb/create_table.sql
+++ challenges/mysql/initdb/create_table.sql
@@ -6,6 +6,8 @@ CREATE TABLE IF NOT EXISTS Challenges (
   Challenge_ID INT AUTO_INCREMENT PRIMARY KEY,
   Title VARCHAR(255) NOT NULL,
   Description TEXT NOT NULL,
-  Category VARCHAR(100) NOT NULL
+  Category VARCHAR(100) NOT NULL,
+  C02_emission FLOAT NOT NULL
+  Timeframe INT NOT NULL CHECK (Timeframe <= 10),
+  Icon TEXT NOT NULL
 );
-
