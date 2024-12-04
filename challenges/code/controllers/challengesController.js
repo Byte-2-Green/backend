2125a166af6443891872efd2ec89849c92290063
Alexandra <smir0002@hz.nl>
2024-12-04 13:56:19 +0100

    fixed some isssues with the mysql

--- challenges/code/controllers/challengesController.js
+++ challenges/code/controllers/challengesController.js
@@ -1,6 +1,6 @@
 import db from '../db.js';  // Import the MySQL connection
 
-// Get all Food for Thought
+// Get all Challenges
 export async function test(req, res) {
   const query = 'SELECT * FROM Challenges';  // SQL query to fetch all records
   db.query(query, (err, results) => {
@@ -9,5 +9,27 @@ export async function test(req, res) {
       return res.status(500).json({ error: 'Internal Server Error' });
     }
     res.status(200).json(results);
+  });
+}
+
+/**
+ * Deny a challenge
+ */
+export async function denyChallenge(req, res) {
+  const { Challenge_ID } = req.body;
+
+  // Validate input
+  if (!Challenge_ID) {
+    return res.status(400).json({ error: 'Challenge ID are required.' });
+  }
+
+  const query = 'INSERT INTO denied_challenges (Challenge_ID) VALUES (?, ?)';
+  
+  db.query(query, [Challenge_ID], (err, results) => {
+    if (err) {
+      console.error('Error denying challenge:', err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.status(201).json({ message: 'Challenge denied successfully.' });
   });
 }
--- challenges/code/routes/index.js
+++ challenges/code/routes/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { test} from '../controllers/challengesController.js';
+import { test, denyChallenge } from '../controllers/challengesController.js';
 import cors from 'cors';
+
 const app = express();
 const router = express.Router();
 
 router.get('/challenges', cors(), test);
+
+router.post('/challenges/deny', cors(), denyChallenge);
 
 export default router;
