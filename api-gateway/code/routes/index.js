0a934adcabd0277d766d1a96cfb452363e79a3e5
Wesley <wesley06@zeelandnet.nl>
2024-12-10 14:32:20 +0100

    made api gateway

--- api-gateway/code/routes/index.js
+++ api-gateway/code/routes/index.js
@@ -22,8 +22,19 @@ const challengeProxy = createProxyMiddleware({
   },
 });
 
+const userProxy = createProxyMiddleware({
+  target:'http://users:3013',
+  changeOrigin: true,
+  onError: (err, req, res) => {
+    console.error(`Error occurred while proxying: ${err.message}`);
+    res.status(502).json({ error: 'Microservice unavailable' });
+  },
+});
+
 router.use('/educational', cors(), educationalProxy);
 
 router.use('/challenges', cors(), challengeProxy);
+
+router.use('/users', cors(), userProxy);
 
 export default router;
