3cfd1554df1691a78d7de48f2fea94443bf9bff9
Wesley <wesley06@zeelandnet.nl>
2024-12-10 10:30:47 +0100

    added base for users microservice

---
+++ users/code/controllers/UserController.js
@@ -0,0 +1,2 @@
+import db from '../db.js';  // Import the MySQL connection
+
---
+++ users/code/routes/index.js
@@ -0,0 +1,20 @@
+
+import express from 'express';
+import { } from '../controllers/UserController.js';
+import swaggerUi from "swagger-ui-express";
+import { checkIfWork } from '../middleware/UserMiddelware.js';
+import swaggerSpec from "./swagger-config.js";
+import cors from 'cors';
+
+const app = express();
+const router = express.Router();
+
+// Serve the Swagger UI at /api-docs
+router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Enable CORS
+router.use(cors());
+
+
+
+export default router;
