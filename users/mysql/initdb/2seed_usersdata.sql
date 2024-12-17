20431dd861e7ec17c4eaf87aee419fb878299826
lexa24lexa <smir0002@hz.nl>
2024-12-17 11:49:43 +0100

    seed user table

--- users/mysql/initdb/2seed_usersdata.sql
+++ users/mysql/initdb/2seed_usersdata.sql
@@ -1,7 +1,5 @@
-USE users_db;
-
-INSERT INTO Users (`full_name`, `username`, `email`, `password_hash`) VALUES
-('John Doe', 'johndoe', 'johndoe@example.com', 'hashed_password_1'),
-('Jane Smith', 'janesmith', 'janesmith@example.com', 'hashed_password_2'),
-('Alice Johnson', 'alicej', 'alicej@example.com', 'hashed_password_3'),
-('Bob Brown', 'bobb', 'bobb@example.com', 'hashed_password_4');
+INSERT INTO Users (Full_name, Username, Email, Password_hash) VALUES
+('John Doe', 'johndoe', 'johndoe@example.com', '1111'),
+('Jane Smith', 'janesmith', 'janesmith@example.com', '2222'),
+('Alice Johnson', 'alicej', 'alicej@example.com', '3333'),
+('Bob Brown', 'bobb', 'bobb@example.com', '4444');
