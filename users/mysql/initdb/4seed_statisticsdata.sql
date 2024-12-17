cc4135f87e255143b55ed1a28eee54fc28508b20
lexa24lexa <smir0002@hz.nl>
2024-12-17 11:51:43 +0100

    seed statistics table

--- users/mysql/initdb/4seed_statisticsdata.sql
+++ users/mysql/initdb/4seed_statisticsdata.sql
@@ -1,4 +1,3 @@
-USE users_db;
-
-INSERT INTO Statistics (`Stat_name`, `Stat_value`, `User_ID`) VALUES ('Co2 Saved', 0, 1),
-('Challenges Completed', 0, 1)
+INSERT INTO Statistics (Stat_name, Stat_value, User_ID) VALUES
+('Co2 Saved', 0, 1),
+('Challenges Completed', 0, 1);
